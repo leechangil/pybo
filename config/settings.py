bcc891ada5ad3e54da6087dc08fd04d34340c165
이창일 <leechangil12@gmail.com>
2020-12-09 11:08:53 +0900

    ALLOWD_HOSTS 변경~

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'kcv^_s@gl$c*&4bstov4*gjiq1g$lnuo0%f49av-211*mb(39u'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in production!!
 DEBUG = True
 
 ALLOWED_HOSTS = ['3.35.84.156']
